cfe68e626e4dec173a9c170a38615d03dd9a3c6c
LucasDev13 <lucaspontes140@gmail.com>
2021-11-09 10:37:14 -0300

    Implementado a classe Calculator.

---
+++ Calculator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora
+{
+    class Calculator
+    {
+        public static double DoOperation(double num1, double num2, string op)
+        {
+            // Default value is "not-a-number" if an operation, such as division, could result in an error.
+            double result = double.NaN;
+
+            //Use a switch statement to do the match.
+            switch (Console.ReadLine())
+            {
+                case "a":
+                    result = num1 + num2;
+                    break;
+                case "s":
+                    result= num1 - num2 ;
+                    break;
+                case "m":
+                    result = num1 * num2;
+                    break;
+                case "d":
+                    // Ask the user to enter a non-zero divisor until they do so.
+                    if(num2 != 0)
+                    {
+                        result = num1 / num2;
+                    }
+                    break;
+                // Return text for an incorrect option entry.
+                default:
+                    break;
+            }
+            return result;
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -30,28 +30,7 @@ namespace Calculadora
             Console.WriteLine("\td - Divide");
             Console.Write("Your option? ");
 
-            //Use a switch statement to do the match.
-            switch (Console.ReadLine())
-            {
-                case "a":
-                    Console.WriteLine($"Your result: {num1} + {num2} = " + (num1 + num2));
-                    break;
-                case "s":
-                    Console.WriteLine($"Your result: {num1} - {num2} = {(num1 + num2)}");
-                    break;
-                case "m":
-                    Console.WriteLine($"Your result: {num1} * {num2} = {(num1 * num2)}");
-                    break;
-                case "d":
-                    // Ask the user to enter a non-zero divisor until they do so.
-                    while (num2 == 0)
-                    {
-                        Console.WriteLine("Enter a non-zero divisor: ");
-                        num2 = Convert.ToInt32(Console.ReadLine());
-                    }
-                    Console.WriteLine($"Your result: {num1} / {num2} = {(num1 / num2)}");
-                    break;
-            }
+            
             // Wait for the user to respond before closing.
             Console.Write("Press any key to close the Calculator console app...");
             Console.ReadKey();
